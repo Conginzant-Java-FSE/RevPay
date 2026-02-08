4debf9392eb4b5b6401b51aafec48e1020b1dbff
vsmidhun21 <midhun890390@gmail.com>
2026-02-08 15:31:44 +0530

    adding properties from env

--- src/main/java/com/revpay/revpay/RevPayApplication.java
+++ src/main/java/com/revpay/revpay/RevPayApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RevPayApplication {
 
     public static void main(String[] args) {
+        //System.out.println("DB_URL = " + System.getenv("DB_NAME"));
+        //System.out.println("DB_USERNAME = " + System.getenv("DB_USER"));
+        //System.out.println("DB_PASSWORD = " + System.getenv("DB_PASS"));
         SpringApplication.run(RevPayApplication.class, args);
     }
 
