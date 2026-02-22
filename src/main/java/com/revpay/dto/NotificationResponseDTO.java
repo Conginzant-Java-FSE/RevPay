48f840fd084bf086663d0ee6fea3337a39185992
vsmidhun21 <midhun890390@gmail.com>
2026-02-22 15:40:45 +0530

    fixes

--- src/main/java/com/revpay/dto/NotificationResponseDTO.java
+++ src/main/java/com/revpay/dto/NotificationResponseDTO.java
@@ -1,62 +1,21 @@
 package com.revpay.dto;
 
+import com.revpay.enums.NotificationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotificationResponseDTO {
     private Long notificationId;
     private String message;
-    private String type;
+    private NotificationType type;
     private Boolean isRead;
     private LocalDateTime createdAt;
-
-    public NotificationResponseDTO() {}
-
-    public NotificationResponseDTO(Long notificationId, String message, String type, Boolean isRead,
-            LocalDateTime createdAt) {
-        this.notificationId = notificationId;
-        this.message = message;
-        this.type = type;
-        this.isRead = isRead;
-        this.createdAt = createdAt;
-    }
-
-    public Long getNotificationId() {
-        return notificationId;
-    }
-
-    public void setNotificationId(Long notificationId) {
-        this.notificationId = notificationId;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Boolean getRead() {
-        return isRead;
-    }
-
-    public void setRead(Boolean read) {
-        isRead = read;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
 }
